96cab4d64c19490336d444db45f3c5aa99d338d0
NicholasMercy <Nicholasnaicker@hotmail.com>
2020-09-14 02:24:10 +0200

    Update Dialouge Systems from Prototype

---
+++ Fatigue/Assets/Scripts/ConversationHandler.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConversationHandler : MonoBehaviour
+{
+    //Declerations
+    public TextAsset jsonFile;
+    Characters characterinJson;
+    LinkedList List;
+
+    //Ui
+    public Text Name;
+    public Text DiaResponse;
+    int count;
+    bool MainDone = false;
+
+    void Start()
+    {
+        //getting json file
+        characterinJson = JsonUtility.FromJson<Characters>(jsonFile.text);
+
+        List = PopulateList(7);
+        //List.MoveToIndex(4);
+        //Debug.Log(List.DisplayCurrentNode().dialogue);
+        List.Print();
+    }
+
+    public void Continue()
+    {
+        //stop routine
+        StopAllCoroutines();
+        //stop dialouge when ended
+        if (List.DisplayNextNode() != null)
+        {
+            count++;
+            if (count == 1)
+            {
+                Name.color = Color.blue;
+                Name.text = List.DisplayCurrentNode().MainTag;
+                DiaResponse.color = Color.blue;
+                StartCoroutine(MainSentence(List.DisplayCurrentNode().response));
+
+            }
+            else if (count == 2)
+            {
+                Name.color = Color.red;
+                Name.text = List.DisplayCurrentNode().NpcTag;
+                DiaResponse.color = Color.red;
+                StartCoroutine(MainSentence(List.DisplayCurrentNode().dialogue));
+                List.MoveToNext();
+                count = 0;
+            }
+        }
+
+
+
+    }
+
+    public LinkedList PopulateList(int index)
+    {
+        LinkedList List = new LinkedList();
+        int count = 0;
+        while (count <= index - 1)
+        {
+            List.AddAtEnd(characterinJson.Mother[count]);
+            count++;
+        }
+        return List;
+    }
+
+    IEnumerator MainSentence(string sentence)
+    {
+        DiaResponse.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            DiaResponse.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
+
+//Character Traits
+[System.Serializable]
+public class Dialogue
+{
+    public int id;
+    public string NpcTag;
+    public string MainTag;
+    public string response;
+    public string dialogue;
+    public string[] tree;
+    public int reqInv;
+    public string secretDia;
+
+
+}
+[System.Serializable]
+//List of Characters (allows for different level list if needed)//
+public class Characters
+{
+
+    public Dialogue[] Father;
+    public Dialogue[] Mother;
+}
---
+++ Fatigue/Assets/Scripts/LinkedListSystem.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkedListSystem
+{
+    public LinkedListSystem next;
+    public Dialogue Dia;
+    public LinkedListSystem(Dialogue dia)
+    {
+        this.Dia = dia;
+
+    }
+    public Dialogue DisplayCurrentNode()
+    {
+        return this.Dia;
+    }
+
+    public void Print()
+    {
+
+        Debug.Log(Dia.NpcTag);
+        if (next != null)
+        {
+            next.Print();
+        }
+    }
+}
+public class LinkedList
+{
+    LinkedListSystem head;
+
+    public void AddAtEnd(Dialogue dia)
+    {
+        if (head == null)
+        {
+            head = new LinkedListSystem(dia);
+            return;
+        }
+
+        LinkedListSystem current = head;
+        while (current.next != null)
+        {
+            current = current.next;
+        }
+        current.next = new LinkedListSystem(dia);
+    }
+
+    public void AddAtStart(Dialogue dia)
+    {
+        LinkedListSystem newhead = new LinkedListSystem(dia);
+        newhead.next = head;
+        head = newhead;
+    }
+
+    public void DeleteValue(Dialogue dia)
+    {
+        if (head == null) return;
+        if (head.Dia == dia)
+        {
+            head = head.next;
+            return;
+        }
+
+        LinkedListSystem current = head;
+        while (current.next != null)
+        {
+            if (current.next.Dia == dia)
+            {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+
+    }
+
+    public void MoveToIndex(int id)
+    {
+        if (head == null) return;
+        if (head.Dia.id == id)
+        {
+            return;
+        }
+
+        LinkedListSystem current = head;
+        while (current.next != null)
+        {
+            if (current.next.Dia.id == id)
+            {
+                head = current.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public void MoveToNext()
+    {
+
+        if (head.next != null)
+        {
+            head = head.next;
+
+        }
+        return;
+    }
+
+    public Dialogue DisplayNextNode()
+    {
+        if (head.next != null)
+        {
+            return head.next.Dia;
+
+        }
+        return null;
+
+    }
+    public void Print()
+    {
+        if (head != null)
+        {
+            head.Print();
+        }
+    }
+
+    public Dialogue DisplayCurrentNode()
+    {
+        if (head != null)
+        {
+            return head.DisplayCurrentNode();
+
+        }
+        return null;
+    }
+
+
+}
